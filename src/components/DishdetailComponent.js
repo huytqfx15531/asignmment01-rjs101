4d13a2dcdefa03a2c3a7a59c37c79e279ac82cad
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-04-06 10:23:23 +0700

    lab04_01

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -32,7 +32,7 @@ class DishDetail extends Component {
                         {comments.map((comment) => {
                             return (
                                 <li key={comment.id}>
-                                    <p>{comment.comment}</p>
+                                    <p>{comment.comment} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                                     <p>-- {comment.author}, {comment.date}</p>
                                 </li>
                             );
