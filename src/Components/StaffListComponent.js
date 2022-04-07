c8eabbda3deeb987596cc3730c54788ccb4f00f8
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-04-07 21:37:43 +0700

    sửa trang stafflisst

--- src/Components/StaffListComponent.js
+++ src/Components/StaffListComponent.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Card, CardImg, CardBody, CardSubtitle } from "reactstrap";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Presentational component (const) dùng để Render danh sách từng nhân viên
-const RenderStaffItem = ({staff}) => {
-    return(
+const RenderStaffItem = ({ staff }) => {
+    return (
         <Link to={`/staff/${staff.id}`}>
             <Card>
                 <CardImg width="100%" src={staff.image} alt={staff.name} />
@@ -24,34 +24,30 @@ const StaffList = (props) => {
     const [sortId] = useState(false);
 
     //Dùng hàm map để kéo toàn bộ mảng ra màn hình, có sử dụng hàm sort để sắp xếp, hàm filter để lọc tìm kiếm
-    const staffList = props.staffs.sort((a,b)=>sortId?a.id : b.id )
-    .filter((val)=>{
-        if(name === "")
-            return val;
-        else if(val.name.toLowerCase().includes(name.toLowerCase()))
-            return val;        
-        return 0;
-    }).map((val) => {
-        return(
-            <div className={column} key={val.id}>
-                <RenderStaffItem staff={val} />
-            </div>
-        );
-    });
+    const staffList = props.staffs.sort((a, b) => sortId ? a.id : b.id)
+        .filter((val) => {
+            if (name === "")
+                return val;
+            else if (val.name.toLowerCase().includes(name.toLowerCase()))
+                return val;
+            return 0;
+        }).map((val) => {
+            return (
+                <div className={column} key={val.id}>
+                    <RenderStaffItem staff={val} />
+                </div>
+            );
+        });
 
     //Render giao diện Staff list
     return (
         <div className="container">
             <div className="row">
                 <div className="col-3 mt-3">
-                    <h3>Nhân viên</h3>                    
+                    <h3>Nhân viên</h3>
                 </div>
-                              
-            </div>            
-                <div className="col-12">
-                    <hr />
-                </div>
-            
+                <hr />
+            </div>
             <div className="row shadow mb-5 mt-5">
                 {staffList}
             </div>
