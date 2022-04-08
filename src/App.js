9c2164f91bcd762ea790c14fe3e557ae4d608a98
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-04-08 20:29:18 +0700

    Intro to Redux

--- src/App.js
+++ src/App.js
@@ -6,22 +6,23 @@ import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+  
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
     
     );
   }
