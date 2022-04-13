3fb423016d77e53510100d5fe9bb50997747b19f
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-04-13 22:00:57 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 import { addComment } from "../redux/ActionCreators";
 import { Control, LocalForm} from "react-redux-form";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderDish({dish}) {
         return(
             <div className="col-12 col-md-5 m-1">
                 <Card>
-                    <CardImg width="100%" src={dish.image} value={dish.name} />
+                    <CardImg width="100%" src={baseUrl + dish.image} value={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
