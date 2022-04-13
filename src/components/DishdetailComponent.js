0a24121cc9e5f13a3301291df07006b31ba7e985
huytqFX15531 <huytqFX15531@funix.edu.vn>
2022-04-13 09:16:09 +0700

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,8 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { addComment } from "../redux/ActionCreators";
 import { Control, LocalForm} from "react-redux-form";
-
-
+import { Loading } from './LoadingComponent';
 
     function RenderDish({dish}) {
         return(
@@ -128,7 +127,25 @@ import { Control, LocalForm} from "react-redux-form";
     }
 
     const DishDetail = (props) => {
-        if(props.dish != null){
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            )
+        }
+        else if(props.errMess){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>                
+            )
+        }
+        else if(props.dish != null){
             return(
                 <div className="container">
                     <div className="row">
